54652e31a46ba31a9381026d913d13d557b19797
Alexey2Barkov <alexey2barkov@gmail.com>
2022-11-25 00:03:43 +0300

    first commit

---
+++ gb_barkov_task_1.1.py
@@ -0,0 +1,11 @@
+# Урок 1. Задание 1.
+a = 20
+b = 40
+c = 80
+print(a)
+print(b)
+print(a, c)
+d = int(input('Вставьте число '))
+e = (b + d) // a
+print(e)
+
---
+++ gb_barkov_task_1.3.py
@@ -0,0 +1,7 @@
+# Урок 1. Задание 3.
+n = input('Укажите число n ')
+nn = int(n + n)
+nnn = int(n + n + n)
+n = int(n)
+s = n + nn + nnn
+print(s)

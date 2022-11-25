b76d57ab045347b89eceb695615fb43d41579a70
Alexey2Barkov <alexey2barkov@gmail.com>
2022-11-25 12:26:24 +0300

    first commit

--- gb_barkov_task_1.1.py
+++ gb_barkov_task_1.1.py
@@ -2,10 +2,10 @@
 a = 20
 b = 40
 c = 80
+n = 'Alex'
 print(a)
 print(b)
 print(a, c)
-d = int(input('Вставьте число '))
-e = (b + d) // a
-print(e)
-
+d = int(input(f'{n}, Укажите ваш возраст '))
+e = (b + d) / a
+print(f'{e:.2f}')
